25047374267509d0633e518be185db5c4dfedfdc
christian <pirvu.ionut7@gmail.com>
2023-05-09 15:18:53 +0300

    stop hosting nvchad

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,12 @@
-require "core"
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+vim.opt.conceallevel = 3
+vim.g.undotree_WindowLayout = 3
 
-local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
-
-if custom_init_path then
-  dofile(custom_init_path)
-end
-
-require("core.utils").load_mappings()
-
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
--- bootstrap lazy.nvim!
-if not vim.loop.fs_stat(lazypath) then
-  require("core.bootstrap").gen_chadrc_template()
-  require("core.bootstrap").lazy(lazypath)
-end
-
-dofile(vim.g.base46_cache .. "defaults")
-vim.opt.rtp:prepend(lazypath)
-require "plugins"
+-- flash on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank({higroup = 'Visual', timeout = 200})
+    vim.opt_local.buflisted = false
+  end,
+})
