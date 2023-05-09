75b5dfb3dfd228efabac6713708444af07cb3911
christian <pirvu.ionut7@gmail.com>
2023-05-09 16:15:59 +0300

    use proper hilight on yank

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,9 +4,9 @@ vim.opt.conceallevel = 3
 vim.g.undotree_WindowLayout = 3
 
 -- flash on yank
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank({higroup = 'Visual', timeout = 200})
-    vim.opt_local.buflisted = false
-  end,
-})
+vim.cmd[[
+augroup highlight_yank
+autocmd!
+au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
+augroup END
+]]
